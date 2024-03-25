df28fac5f09521d18fedf33ef771b3740d9aec1e
MeorMeow <152157961+MeorMeow@users.noreply.github.com>
2024-03-25 10:14:53 +0200

    Update kontr.py

--- Pārbaudes darbs python/kontr.py
+++ Pārbaudes darbs python/kontr.py
@@ -1,10 +1,29 @@
+print("Sveiki! Šajā spēlē tev vajadzēs censties uzminēt 5 skaitļus no 1 līdz 50 vai 100.")
+print("Katrs veiktais mēģinājums uzminēt - 2 punkti")
+print("Ja spēlē no 1 līdz 50 tad:"
+"Uzminot 1 skaitli + 5, "
+"Uzminot 2 skaitļus + 10, "
+"Uzminot 3 skaitļus + 20, "
+"Uzminot 4 skaitļus + 40, "
+"Uzminot 5 skaitļus + 50, "
+)
+print("Ja spēlē no 1 līdz 100 tad:")
+print( 
+"Uzminot 1 skaitli + 10, "
+"Uzminot 2 skaitļus + 20, "
+"Uzminot 3 skaitļus + 60, "
+"Uzminot 4 skaitļus + 100, "
+"Uzminot 5 skaitļus + 200, "
+)
+
 import random
 
-sākums = input(f"Gribi 50 vai 100 ciparus?")
+sākums = input(f"Vai tu gribi minēt no 50 vai 100 cipariem?")
 if sākums == "50": 
-    random_numbers = [random.randint(1, 50) for _ in range(5)] 
-    secība = sorted(random_numbers)
+    pakape = [random.randint(1, 50) for _ in range(5)] 
+    secība = sorted(pakape)
 elif sākums == "100": 
-    random_numbers = [random.randint(1, 100) for _ in range(5)]
-    secība = sorted(random_numbers)
+    pakape = [random.randint(1, 100) for _ in range(5)]
+    secība = sorted(pakape)
 print(secība)
+
