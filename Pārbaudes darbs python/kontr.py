7b84766b6729bea0219fffc59c48a9a9d8b6d054
MeorMeow <152157961+MeorMeow@users.noreply.github.com>
2024-03-29 19:26:27 +0200

    Update kontr.py

--- Pārbaudes darbs python/kontr.py
+++ Pārbaudes darbs python/kontr.py
@@ -1,66 +1,45 @@
-print("Sveiki! Šajā spēlē tev vajadzēs censties uzminēt 5 skaitļus no 1 līdz 50 vai 100.")
+print("\nSveiki! Šajā spēlē tev vajadzēs censties uzminēt 5 skaitļus no 1 līdz 50 vai 100. Skaitļi var atkārtoties.")
 print("Katrs veiktais mēģinājums uzminēt - 2 punkti")
 print("Ja spēlē no 1 līdz 50 tad:"
-"Uzminot 1 skaitli + 5, "
-"Uzminot 2 skaitļus + 10, "
-"Uzminot 3 skaitļus + 20, "
-"Uzminot 4 skaitļus + 40, "
-"Uzminot 5 skaitļus + 50, "
+"Uzminot skaitli + 50"
 )
 print("Ja spēlē no 1 līdz 100 tad:")
 print( 
-"Uzminot 1 skaitli + 10, "
-"Uzminot 2 skaitļus + 20, "
-"Uzminot 3 skaitļus + 60, "
-"Uzminot 4 skaitļus + 100, "
-"Uzminot 5 skaitļus + 200, "
+"Uzminot skaitli + 100 punkti "
 )
 
 import random
 
-sākums = input(f"Vai tu gribi minēt no 50 vai 100 cipariem?")
-if sākums == "50": 
-    pakape = [random.randint(1, 50) for _ in range(5)] 
-    secība = sorted(pakape)
-elif sākums == "100": 
-    pakape = [random.randint(1, 100) for _ in range(5)]
-    secība = sorted(pakape)
 
-
-
-def minejums2():
-    att = 2 
-    minejums2 = "" 
-    while att > 1:
-        if minejums2 not in range(1,51):
-            att + 0
-            print("Tev vajag ievadīt no 1 līdz 50.")
-        else:
-            att-=1
-           
-        minejums2 = int(input("Mēģini uzminēt 5 skaitļus111:"))  
-        
-print(minejums2())
-
-
-
-
-# Guessing game no 1 - 10
-
-import random
+pakapes_izvele= int(input("Izvēlies grūtības pakāpi 50 vai 100: "))
+def sakums():
+    if pakapes_izvele == "50": 
+        rand_skaitlis = [random.randint(1, 51)] 
     
-num = random.randint(1, 10)
-guess = None
-    
-while guess != num:
-    guess = input("guess a number between 1 and 10: ")
-    guess = int(guess)
-    
-    if guess == num:
-        print("congratulations! you won!")
-        break
+    elif pakapes_izvele == "100": 
+        rand_skaitlis = [random.randint(1, 101)]
     else:
-        print("nope, sorry. try again!")
+        print("Ieraksti 50 vai 100.")
+    
+
+
+def minejums():
+    
+      
+    while True:
+        minējums = int(input("Ieraksti skaitli:")) 
+         
+        if minējums > 1 and minējums < pakapes_izvele:
+            print(minējums)
+            break
+        
+        else:
+            print(f"Minējumam jābūt ietvaros no 1 līdz {pakapes_izvele}.")
+            
+            
+
+
+print(minejums())
 
 
 
