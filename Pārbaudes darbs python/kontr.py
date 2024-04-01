ac3f535968bcf94f5eed287b8d3a676e7d3a4137
MeorMeow <152157961+MeorMeow@users.noreply.github.com>
2024-04-01 12:49:38 +0300

    Update kontr.py

--- Pārbaudes darbs python/kontr.py
+++ Pārbaudes darbs python/kontr.py
@@ -1,36 +1,36 @@
-print("\nSveiki! Šajā spēlē tev vajadzēs censties uzminēt skaitli no 1 līdz 50 vai 100. Skaitļi var atkārtoties.")
-print("Katrs veiktais mēģinājums uzminēt - 2 punkti")
-print("Ja spēlē no 1 līdz 50 tad:"
-"Uzminot skaitli + 50"
-)
+print("\nSveiki! Šajā spēlē tev vajadzēs censties uzminēt skaitli no 1 līdz 50 vai 100.")
+print("Katrs veiktais mēģinājums uzminēt -2 punkti.")
+
+print("\nJa spēlē no 1 līdz 50 tad:")
+print("Uzminot skaitli + 50")
+
 print("Ja spēlē no 1 līdz 100 tad:")
-print( 
-"Uzminot skaitli + 100 punkti "
-)
+print("Uzminot skaitli + 100 punkti ")
+
+print("\nJa vēlies pabeigt spēli vai nomainīt gŗūtību, tad veicot minējumu ieraksi 0.")
 
 import random
 
-
-
 def pakape():
+    
     while True:
-        
         pakapes_izvele = input("\nIzvēlies grūtības pakāpi 50 vai 100: ")
+        
         if pakapes_izvele in ['50', '100']:
             return int(pakapes_izvele)
-
-    
+        
         else:
             print("Tev jāizvēlas 50 vai 100. Mēģini vēlreiz.")
 
 
+
 def minējums(max_skaitlis):  
+    punktu_skaits = 0
+    
     while True:
-        punktu_skaits = 0
         random_skaitlis = random.randint(1, max_skaitlis)
         mans_minejums = int(input("\nTavs minējums ir: "))
 
-    
         if mans_minejums == random_skaitlis:
             if max_skaitlis == 50:
                 punktu_skaits += 50
@@ -47,20 +47,17 @@ def minējums(max_skaitlis):
             punktu_skaits -= 2
         
         print(f"Tavs punktu skaits: {punktu_skaits}.")
-        return punktu_skaits
- 
 
-def lai_stradātu():
-  
-    grūtības_pakāpe = pakape()
-    if grūtības_pakāpe == 50:
-        punktu_skaits = minējums(50) 
-        
-    else:
-        punktu_skaits = minējums(100)
-    return punktu_skaits
 
+def lai_stradātu():  
     
-lai_stradātu()
+    while True:
+        grūtības_pakāpe = pakape()
+        
+        if grūtības_pakāpe == 50:
+             minējums(50) 
+        else:
+             minējums(100)
 
+lai_stradātu()
 
